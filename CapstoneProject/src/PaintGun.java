b1326c0d50185fdbfd352f76cedb7556f233145e
rvenkatesh335 <rvenkatesh335@student.fuhsd.org>
2022-05-03 15:11:35 -0700

    imports

--- CapstoneProject/src/PaintGun.java
+++ CapstoneProject/src/PaintGun.java
@@ -1,4 +1,4 @@
 
-public class PaintGun {
-
+public class PaintGun extends Sprite {
+	private 
 }
---
+++ CapstoneProject/src/sprites/Mario.java
@@ -0,0 +1,53 @@
+package sprites;
+
+
+import java.util.List;
+
+import processing.core.PImage;
+
+public class Mario extends Sprite {
+
+	public static final int MARIO_WIDTH = 40;
+	public static final int MARIO_HEIGHT = 60;
+	public static final double GRAVITY = 0.6;
+
+	private double xVel, yVel;
+	private boolean onPlatform;
+
+	public Mario(PImage img, int x, int y) {
+		super(img, x, y, MARIO_WIDTH, MARIO_HEIGHT);
+		xVel = 0;
+		yVel = 0;
+	}
+
+	// METHODS
+	public void walk(int dir) {
+		xVel =   dir;
+	}
+
+	public void jump() {
+		yVel -= 1.6;
+	}
+
+	public void act(List<Sprite> obstacles) {
+		
+		yVel += GRAVITY; // gravity
+		
+		x += xVel;
+		y += yVel;
+		
+		for (Sprite sprite : obstacles) {
+			if (super.intersects(sprite)) {
+				yVel = 0;
+				super.y = sprite.y - super.height;
+				onPlatform = true;
+			}
+		}
+		if (yVel != 0) {
+			onPlatform = false;
+		}
+		
+	}
+
+
+}
