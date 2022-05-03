83d9321c466fc61896870db145bb42a6cc5ead00
rvenkatesh335 <rvenkatesh335@student.fuhsd.org>
2022-05-03 15:15:10 -0700

    bruh wait

--- CapstoneProject/src/Avatar.java
+++ CapstoneProject/src/Avatar.java
@@ -1,10 +1,18 @@
 import java.awt.Color;
 import java.util.ArrayList;
 
+import sprites.Sprite;
+
 public class Avatar extends Sprite {
 	private PaintGun gun;
 	private ArrayList<PaintBomb> arsenal;
 	private Color playerColor;
 	private int health, speed;
 	private int baseX, baseY;
+	
+	public Avatar(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+	
 }
--- CapstoneProject/src/PaintGun.java
+++ CapstoneProject/src/PaintGun.java
@@ -1,4 +1,10 @@
+import sprites.Sprite;
 
 public class PaintGun extends Sprite {
-	private 
+	private static final boolean aboutToPunchYouInTheFace = true;
+	
+	public PaintGun(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
 }
