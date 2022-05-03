9c1f2bd0a86182f5fbf3e2047a709a4a9b61028e
Ishaan Singh <isingh084@student.fuhsd.org>
2022-05-03 15:06:54 -0700

    Draft

---
+++ CapstoneProject/src/Avatar.java
@@ -0,0 +1,10 @@
+import java.awt.Color;
+import java.util.ArrayList;
+
+public class Avatar extends Sprite {
+	private PaintGun gun;
+	private ArrayList<PaintBomb> arsenal;
+	private Color playerColor;
+	private int health, speed;
+	private int baseX, baseY;
+}
