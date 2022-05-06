394f948ce0185a06ce88a3783371a6581211be96
rvenkatesh335 <rvenkatesh335@student.fuhsd.org>
2022-05-06 15:17:42 -0700

    pre-alpha test

--- CapstoneProject/src/sprites/PaintGun.java
+++ CapstoneProject/src/sprites/PaintGun.java
@@ -1,8 +1,10 @@
 package sprites;
 
+import java.awt.geom.Point2D;
+
 public class PaintGun extends Sprite {
-	int ammo, reloadTime, velocity;
-	PaintBlock stroke;
+	private Avatar owner;
+	private int ammo, reloadTime, velocity, stroke;
 	
 	public PaintGun(Avatar a, int capacity, int rTime, int vel, PaintBlock str) {
 		super((int)a.x, (int)a.y, (int)a.width, (int)a.height);
@@ -10,12 +12,14 @@ public class PaintGun extends Sprite {
 		ammo = capacity;
 		reloadTime = rTime;
 		velocity = vel;
-		
-		stroke = str;
 	}
 
-	public void shoot() {
-		// TODO Auto-generated method stub
+	public void shoot(Point2D point) {
+		if(ammo > 0) {
+			PaintBlock bullet = new PaintBlock(owner, stroke, velocity, point);
+			
+			ammo--;
+		}
 		
 	}
 
