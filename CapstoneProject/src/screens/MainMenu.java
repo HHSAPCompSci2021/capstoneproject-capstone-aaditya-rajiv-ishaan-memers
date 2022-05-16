eb9c81387f6a305538d00637b08937cb7bf99dc3
rvenkatesh335 <rvenkatesh335@student.fuhsd.org>
2022-05-15 19:04:16 -0700

    orange is the best color

--- CapstoneProject/src/screens/MainMenu.java
+++ CapstoneProject/src/screens/MainMenu.java
@@ -28,11 +28,11 @@ public class MainMenu extends Screen {
 
 	public void draw() {
 
-		surface.background(255,255,255);
+		surface.background(255,100,0);
 		
 		surface.rect(button.x, button.y, button.width, button.height, 10, 10, 10, 10);
 		surface.fill(0);
-		String str = "Click me!";
+		String str = "Begin Game!";
 		float w = surface.textWidth(str);
 		surface.text(str, button.x+button.width/2-w/2, button.y+button.height/2);
 		
