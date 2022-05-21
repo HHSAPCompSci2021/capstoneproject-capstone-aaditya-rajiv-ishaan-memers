8c5c90b0113802f04836fb1a086e380853660bc9
Ishaan Singh <isingh084@student.fuhsd.org>
2022-05-21 01:05:26 -0700

    Fixed condition, if flag captured, it does more damage

--- CapstoneProject/src/sprites/Avatar.java
+++ CapstoneProject/src/sprites/Avatar.java
@@ -174,7 +174,7 @@ public class Avatar extends Sprite {
 		gun = new PaintGun((int) (x + 200), (int) (y + 150), 50, 100, 5, PaintBlock.VELOCITY, PaintBlock.LENGTH);
 		numDeaths++;
 	}
-<<<<<<< Updated upstream
+
 	
 	public void reset() {
 		super.moveToLocation(baseX, baseY);
